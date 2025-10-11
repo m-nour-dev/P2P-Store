1f90b3b1edd0af8999151cd5a6c752a01e824bf8
Mouhamad faruk <132808834+mouhamadfarukalshikhwiss@users.noreply.github.com>
2025-10-11 03:11:16 +0300

    add_ flutter_stripe_package

--- android/app/src/main/kotlin/com/example/p2p_store/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/p2p_store/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.p2p_store
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+}
