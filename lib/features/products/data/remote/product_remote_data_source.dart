2d45f05b97c754cae12280c1df8fb23dd6b2457c
Muhammed Nour <m.noru.almsto@gmail.com>
2025-10-12 22:35:36 +0300

    added ProductRemoteDataSource file

---
+++ lib/features/products/data/remote/product_remote_data_source.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:p2p_store/features/products/data/models/product_models.dart';
+import 'package:http/http.dart' as http;
+
+class ProductRemoteDataSource {
+  final String basUrl = 'https://dummyjson.com';
+  Future<ProductListModel> getProducts() async {
+    try {
+      final response = await http.get(Uri.parse('https://dummyjson.com'));
+
+      if (response.statusCode == 200) {
+        final jsonData = jsonDecode(response.body) as Map<String, dynamic>;
+        return ProductListModel.fromJson(jsonData);
+      } else {
+        throw Exception('Server connection failed! ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error while fethcing data! $e');
+    }
+  }
+}
