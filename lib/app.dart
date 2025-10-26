15a598f7b90c1d41f69d9abddbc6990449662ab8
Eyad107Hussein <m.eyad.hussein2003@gmail.com>
2025-10-26 03:30:26 +0300

    ..

--- lib/app.dart
+++ lib/app.dart
@@ -4,6 +4,9 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:p2p_store/core/constants/app_constant.dart';
 import 'package:p2p_store/core/theme/app_theme.dart';
 import 'package:p2p_store/features/authentication/presentation/pages/splash_screen.dart';
+import 'package:p2p_store/features/products/presentation/manager/theme_cubit.dart';
+import 'package:p2p_store/features/products/presentation/manager/theme_state.dart';
+import 'package:p2p_store/features/products/presentation/widgets/main_layout.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -15,11 +18,18 @@ class App extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
-        return MaterialApp(
-          title: AppConstants.appName,
-          theme: AppTheme.lightTheme,
-          debugShowCheckedModeBanner: false,
-          home: SplashScreen(),
+        return BlocProvider(
+          create: (_) => ThemeCubit(),
+          child: BlocBuilder<ThemeCubit, ThemeState>(
+            builder: (context, state) => MaterialApp(
+              title: AppConstants.appName,
+              theme: context.read<ThemeCubit>().state.isDarkToggled
+                  ? AppTheme.darkTheme
+                  : AppTheme.lightTheme,
+              debugShowCheckedModeBanner: false,
+              home: SplashScreen(),
+            ),
+          ),
         );
       },
     );
